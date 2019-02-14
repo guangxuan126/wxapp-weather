c6f7b27495307116125f6e606c555c797f07ecfb
GuangXuan <tlfggx@gmail.com>
2019-02-14 14:43:49 +0800

    添加时间背景

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -1,86 +1,98 @@
 // miniprogram/pages/main/main.js
+var util = require('../../utils/util.js');
+var util2 = require('../../utils/util2.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    currentTab: 0,
-    forecast: []
-  },
-  clickTab: function (e) {
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentTab: e.target.dataset.current
-      })
-    }
-  },
-  stopTouchMove: function () {
-    return false;
+    nowTemp: 14,
+    nowWeather: "多云",
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var bgUrlM = '../../images/morning.png';
-    var bgUrlA = '../../images/sun2.png';
-    var bgUrlN = '../../images/night.png';
+    var bgUrlM = 'https://images.gxuann.cn/weather/morning.png';
+    var bgUrlSR = 'https://images.gxuann.cn/weather/sun.png'
+    var bgUrlSS = 'https://images.gxuann.cn/weather/sun2.png';
+    var bgUrlN = 'https://images.gxuann.cn/weather/night.png';
     var that = this;
-    var time = parseInt(new Date().getHours());
-    var times = 1;
-    if (5 < time && time < 17) {
-      times = 1;
-      console.log("morning")
-    }else if (17 <= time && time < 20){
-      times = 2;
-      console.log("afternoon")
-    }else{
-      times = 3;
-      console.log("night")
-    }
-    switch(times){
-      case 1:
-        that.setData({
-          bgUrl: bgUrlM
-        });
-        wx.setNavigationBarColor({
-          frontColor: "#ffffff",
-          backgroundColor: "#4bacd9"
-        });
-        break;
-      case 2:
-        that.setData({
-          bgUrl: bgUrlA
-        });
-        wx.setNavigationBarColor({
-          frontColor: "#ffffff",
-          backgroundColor: "#c66445"
-        });
-        break;
-      case 3:
-        that.setData({
-          bgUrl: bgUrlN
-        });
-        wx.setNavigationBarColor({
-          frontColor: "#ffffff",
-          backgroundColor: "#05111d"
-        });
-        break;
-    }
+    var time = util.formatTime(new Date());
+    var time2 = util2.formatTime(new Date());
+
     wx.request({
       url: 'https://free-api.heweather.com/s6/weather',
       data: {
         location: '北辰区',
         key: '5ddb6ec7a4814816864f58b7afce7110'
       },
-      success: res=> {
-        let data = res.data.HeWeather6[0]
-        console.log(data)
+      success: res => {
+        let result = res.data.HeWeather6[0]
+        let temp = result.now.tmp
+        let weather = result.now.cond_txt
+        let sr = result.daily_forecast[0].sr
+        let ss = result.daily_forecast[0].ss
+        console.log(result)
+        this.setData({
+          nowTemp: temp + '°C',
+          nowWeather: weather,
+        })
+        //时间背景缺少日出日落
+        if (time < sr) {
+          this.setData({
+            bgUrl: bgUrlN
+          });
+          wx.setNavigationBarColor({
+            frontColor: "#ffffff",
+            backgroundColor: "#05111d"
+          });
+        } else {
+          if (time > ss) {
+            this.setData({
+              bgUrl: bgUrlN
+            });
+            wx.setNavigationBarColor({
+              frontColor: "#ffffff",
+              backgroundColor: "#05111d"
+            });
+          } else {
+            if (time > sr && time2 > sr && time < ss) {
+              this.setData({
+                bgUrl: bgUrlM
+              });
+              wx.setNavigationBarColor({
+                frontColor: "#ffffff",
+                backgroundColor: "#4bacd9"
+              });
+            } else {
+              if (time < sr && time2 > sr) {
+                this.setData({
+                  bgUrl: bgUrlSR
+                });
+                wx.setNavigationBarColor({
+                  frontColor: "#ffffff",
+                  backgroundColor: "#315950"
+                });
+              } else {
+                this.setData({
+                  bgUrl: bgUrlSS
+                });
+                wx.setNavigationBarColor({
+                  frontColor: "#ffffff",
+                  backgroundColor: "#c66445"
+                });
+              }
+            }
+          }
+        }
+
       }
     })
+
+    
+    
   },
 
   /**
